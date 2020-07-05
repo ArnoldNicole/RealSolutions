a34824eb5a2d461eb70f1d9476d85c448e9ef510
ArnoldNicole <arnoldwamae2@gmail.com>
2020-07-05 15:26:40 +0300

    Privacy Policy Updated

--- resources/views/policies/policy.blade.php
+++ resources/views/policies/policy.blade.php
@@ -5,8 +5,12 @@
 </head>
 <body>
 	<h2>About this policy</h2>
+	<p>Real solutions is hosted on heroku. All the source code is hosted on github. We do not collect any data from your browsers nor do we use cookies. We do not require that users create accounts on our platform therefore you should feel secure</p>
+	<p>Real solutions does not ask for credit card details nor for passwords</p>
 	<h2>Terms Of use</h2>
+	<p>You are allowed to share our articles on all platforms as long as you add our link to your references. All tutorials and videos shown on our app belog to their copy right owners and permission should be sought before using them</p>
 	<h2>Security update</h2>
-	<h2>ETC</h2>
+	<p>We update our code base every two weeks and we advise users to use the latest version of chrome or Firefox for the best experience on our platfrorm</p>
+	
 </body>
 </html>
